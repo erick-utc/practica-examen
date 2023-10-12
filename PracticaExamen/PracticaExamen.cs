839cd09e2994de4223ae9c2652502fe54e504c08
Erick Espinoza <eespinozaa0489@uvirtual.ac.cr>
2023-10-11 19:05:14 -0600

    agregando proyecto practica examen

---
+++ PracticaExamen/PracticaExamen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaExamen
+{
+    internal class PracticaExamen
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
